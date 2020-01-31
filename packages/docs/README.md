669afcc7be8402029d2f3ab305e357ea04a7c17c
Alex Khomenko <alex.khomenko@getuniverse.com>
2020-01-31 11:56:25 +0200

    Add docs-tools as external package

--- packages/docs/README.md
+++
@@ -1,5 +0,0 @@
-# Docs
-
-These are components for the Universe-Kit Docs.
-
-**These components should not be used in applications!**
--- packages/docs/src/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Metadata } from "./Metadata";
-export { default as ColorExample } from "./ColorExample";
-export { default as BackgroundBox } from "./BackgroundBox";
-export { default as ShadeGrid } from "./ShadeGrid";
-export { default as MarginExample } from "./MarginExample";
-export { default as PaddingExample } from "./PaddingExample";
